d80c67ab0ce6bf6e8478873c93f3e4674f33b2d6
jiangqi30 <591574058@qq.com>
2019-04-28 18:36:53 +0800

    完成公共图标组件support-ico

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,14 @@
+const path = require('path')
+//步骤一:mock数据
+const appData = require('./data.json')
+const seller = appData.seller
+const goods = appData.goods
+const ratings = appData.ratings
+
+function resolve(dir) {
+  return path.join(__dirname, dir)
+}
+
 module.exports = {
   css: {
     loaderOptions: {
@@ -14,5 +25,34 @@ module.exports = {
       postCompile: true,
       theme: true
     }
+  },
+  //步骤二:mock数据
+  devServer: {
+    before(app) {
+      app.get('/api/seller', function (req, res) {
+        res.json({
+          errno: 0,
+          data: seller
+        })
+      })
+      app.get('/api/goods', function (req, res) {
+        res.json({
+          errno: 0,
+          data: goods
+        })
+      })
+      app.get('/api/ratings', function (req, res) {
+        res.json({
+          errno: 0,
+          data: ratings
+        })
+      })
+    }
+  },
+  chainWebpack(config) {
+    //配置自定义路径
+    config.resolve.alias
+      .set('components', resolve('src/components'))
+      .set('common', resolve('src/common'))
   }
 }
